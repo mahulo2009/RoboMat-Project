494c04d5b27b96144557419a839a429cf48311a0
mhuertas <manuel.huertas.lopez@gmail.com>
2018-10-05 06:59:37 +0100

    Autotunning script

--- src/auto_tunning.py
+++ src/auto_tunning.py
@@ -1,36 +1,35 @@
 #!/usr/bin/env python
 
 import rospy
+import roslib; roslib.load_manifest("rosserial_arduino")
+import actionlib
 from geometry_msgs.msg import Vector3
 
+from rosserial_arduino.srv import *
+
+
 def callback(data):
-	
 	target = data.x
 	current = data.y
 	demanded = data.z
-
-
 	rospy.loginfo(target)
 	rospy.loginfo(current)
 	rospy.loginfo(demanded)
 
 def listener():
-	pub = rospy.Publisher('/car/pid_params', Vector3, queue_size=10)
 	rospy.init_node("robot_mat_auto_tunning",anonymous=True)
-	#rospy.Subscriber("/car/pid_telemetry",Vector3,callback)
+	rospy.Subscriber('/car/pid_telemetry_wheel_1_msg', Vector3, callback)
 
-	pid_params = Vector3()
-	pid_params.x = 0.5
-	pid_params.y = 0.0
-	pid_params.z = 0.0
+	rospy.set_param('/robomat/pid_kp', 0.7)
+	rospy.set_param('/robomat/pid_ki', 0.0)	
+	rospy.set_param('/robomat/pid_kd', 0.0)
 
-	rate = rospy.Rate(0.01) # 10hz
-	while not rospy.is_shutdown():
-		pub.publish(pid_params)
-		rate.sleep()
- 
+	rospy.wait_for_service('test_srv')
+	service = rospy.ServiceProxy('test_srv', Test)
+	result = service("hola")
 
-
+	rospy.spin()
+	
 
 if __name__ == '__main__':
 	listener()
